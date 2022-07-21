5943748cee6bacdf5e42e2d8816f2efaf2778d4b
YOGA 520 PENT GOLD <YOGA 520 PENT GOLD@MrRedfox>
2022-07-21 16:23:17 -0500

    Se agregó initComponents y constructor

--- src/miniproyecto3/MiniProyecto3.java
+++ src/miniproyecto3/MiniProyecto3.java
@@ -13,12 +13,32 @@ public class MiniProyecto3 extends JFrame {
     private JButton biniciar, bseguir, brandom;
     private JPanel pnorth, pcenter, psouth;
 
+    
+    /**
+     * Metodo para iniciar y construir la GUI
+     */
+    private void initComponents()
+    {
+        
+    }
+    
+    public MiniProyecto3()
+    {
+        initComponents();
+        setSize(680,720);
+        setVisible(true);
+        setResizable(false);
+        setTitle("FPOE - MiniProjecto3");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+    
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        MiniProyecto3 miO = new MiniProyecto3();
     }
     
 }
