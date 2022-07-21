ca22bf34a53adbce48604923c05c1ef916ecb212
j5434 <j5434@SPARK-LAPTOP>
2022-07-21 16:18:00 -0500

    inicio GUI

--- src/miniproyecto3/MiniProyecto3.java
+++ src/miniproyecto3/MiniProyecto3.java
@@ -1,23 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package miniproyecto3;
+
+import java.awt.BorderLayout;
+import javax.swing.*;
+import java.awt.GridLayout;
 
 /**
  *
  * @author j5434
  */
-public class MiniProyecto3 {
+public class MiniProyecto3 extends JFrame {
+    private JLabel ltitulo;
+    private JButton biniciar, bseguir, brandom;
+    private JPanel pnorth, pcenter, psouth;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hola mundo");
         
-        System.out.println("a mi sí mentiroso");
     }
     
 }
